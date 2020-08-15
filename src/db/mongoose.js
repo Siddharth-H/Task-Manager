affb1271506c11046e1d93831ca66754eee515b1
Siddharth-H <siddharthahatkar@gmail.com>
2020-08-14 23:10:37 -0600

    using mongoose to add users and tasks

---
+++ src/db/mongoose.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        validate(value){
+            if(value < 0){
+                throw new Error('Age must be a positive number')
+            }
+        }
+    }
+})
+
+const me = new User({
+    name: 'Killua',
+    email: 'killua@hxh.com',
+    age: 10
+})
+
+me.save().then(()=>{
+    console.log(me)
+}).catch((error)=>{
+    console.log('Error!, ',error);
+})
+
+// const Task = mongoose.model('Task', {
+//     description: {
+//         type: String
+//     },
+//     completed: {
+//         type: Boolean
+//     }
+// })
+
+// const task = new Task({
+//     description: 'Add tasks',
+//     completed: true
+// })
+
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
